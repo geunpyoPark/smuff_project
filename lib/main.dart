fc6abbaf517f024b79544d70496b25dd18a36508
geunpyoPark <rmse303@gmail.com>
2024-11-16 02:25:59 +0900

    캘린더 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:smuff_project/firebase_options.dart';
 import 'package:smuff_project/screen/login_screen.dart';
 import 'package:smuff_project/screen/signup_screen.dart';
 import 'package:smuff_project/screen/home_screen.dart';
@@ -8,14 +7,19 @@ import 'package:smuff_project/screen/camera_screen.dart';
 import 'package:smuff_project/screen/gallery_screen.dart';
 import 'package:smuff_project/screen/gallerydetail_screen.dart';
 import 'package:smuff_project/screen/setting_screen.dart';
+import 'package:intl/date_symbol_data_local.dart'; // 올바른 intl 패키지 임포트
 
 
-void main() {
+
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized(); // 비동기 함수 호출 전 초기화
+  await initializeDateFormatting('ko_KR', null); // 한국어 로케일 초기화
 
   runApp(
     MaterialApp(
-      // home: home_screen(),
-      home: calendar_screen(),
+      //home: home_screen(),
+      home: CalendarScreen(),
       // home: login_screen(),
       //home: signup_screen(),
       //home: setting_screen(),
--- lib/screen/calendar_screen.dart
+++ lib/screen/calendar_screen.dart
@@ -1,107 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:smuff_project/component/main_calendar.dart';
+import 'package:smuff_project/screen/home_screen.dart'; // home_screen.dart를 임포트
 
-class calendar_screen extends StatelessWidget {
+class CalendarScreen extends StatefulWidget {
+  const CalendarScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CalendarScreen> createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  DateTime selectedDate = DateTime.now();
+
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
+    setState(() {
+      this.selectedDate = selectedDate;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.home, color: Colors.black),
+          onPressed: () {
+            // 홈 화면으로 이동하는 로직
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => home_screen()),
+            );
+          },
+        ),
+      ),
       body: Center(
         child: SizedBox(
           width: 411,
           height: 707,
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 20),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // 캘린더 박스
+                  Center(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      padding: const EdgeInsets.all(8.0),
+                      child: MainCalendar(
+                        selectedDate: selectedDate,
+                        onDaySelected: onDaySelected,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
 
-                // 상단 사이즈박스
-                SizedBox(
-                  height: 50,
-                  child: Row(
+                  // 장소 텍스트와 추가 버튼
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
+                      Text(
+                        '종로 중앙시장 데이트',
+                        style: TextStyle(fontSize: 16),
+                      ),
                       IconButton(
-                        icon: Icon(Icons.home, color: Colors.black),
-                        onPressed: () {},
+                        icon: Icon(Icons.add_box_outlined),
+                        onPressed: () {
+                          // 추가 버튼의 동작 구현
+                        },
                       ),
                     ],
                   ),
-                ),
-                SizedBox(height: 20),
+                  SizedBox(height: 20),
 
-                // 캘린더 박스
-                Center(
-                  child: SizedBox(
-                    width: 300,
-                    height: 200,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(12),
-                      child: Container(
-                        color: Colors.grey[300],
-                        child: GridView.builder(
-                          padding: EdgeInsets.all(10),
-                          itemCount: 31,
-                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 7,
-                            mainAxisSpacing: 2,
-                            crossAxisSpacing: 2,
+                  // 이미지 격자와 지도
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: SizedBox(
+                          height: 150,
+                          child: GridView.builder(
+                            padding: EdgeInsets.all(4),
+                            itemCount: 4,
+                            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              mainAxisSpacing: 4,
+                              crossAxisSpacing: 4,
+                            ),
+                            itemBuilder: (context, index) {
+                              return ClipRRect(
+                                borderRadius: BorderRadius.circular(8),
+                                child: Container(color: Colors.grey[300]),
+                              );
+                            },
                           ),
-                          itemBuilder: (context, index) {
-                            return Center(child: Text('${index + 1}'));
-                          },
                         ),
                       ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10),
-
-                // 장소 텍스트와 추가 버튼
-                SizedBox(
-                  width: double.infinity,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('종로 중앙시장 데이트', style: TextStyle(fontSize: 16)),
-                        IconButton(
-                          icon: Icon(Icons.add_box_outlined),
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20),
-
-                // 이미지 격자와 지도
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: SizedBox(
-                        height: 150,
-                        child: GridView.builder(
-                          padding: EdgeInsets.all(8),
-                          itemCount: 6,
-                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                            mainAxisSpacing: 4,
-                            crossAxisSpacing: 4,
-                          ),
-                          itemBuilder: (context, index) {
-                            return ClipRRect(
-                              borderRadius: BorderRadius.circular(8),
-                              child: Container(color: Colors.grey[300]),
-                            );
-                          },
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: SizedBox(
-                        height: 150,
+                      SizedBox(width: 8),
+                      Expanded(
+                        flex: 1,
                         child: ClipRRect(
                           borderRadius: BorderRadius.circular(8),
                           child: Image.network(
@@ -110,10 +114,10 @@ class calendar_screen extends StatelessWidget {
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
